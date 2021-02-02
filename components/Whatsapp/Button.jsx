79ff5bbfc64a5d13f6fd82feae4311309d1f549b
Christian Escobar <christian.e@rompecabeza.cl>
2021-02-02 00:20:37 -0300

    Add WhatsappBtn

---
+++ components/Whatsapp/Button.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faWhatsapp } from "@fortawesome/free-brands-svg-icons";
+
+function Button(props) {
+  return (
+    <>
+      <a
+        href={`https://wa.me/${props.numero}`}
+        className="fixed bottom-2 left-3 rounded-full bg-gray-900 px-5 py-2 flex place-items-center gap-3 text-white cursor-pointer w-max"
+      >
+        <div className="img h-5">
+          <FontAwesomeIcon icon={faWhatsapp} className="w-full h-full" />
+        </div>
+        <div className="text">Whatsapp</div>
+      </a>
+    </>
+  );
+}
+
+export default Button;
--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import Banner from '../components/Banner/Banner'
 import Footer from '../components/Footer/Footer'
 import Header from '../components/Header/Header'
 import ServiciosGrid from '../components/Servicios/ServiciosGrid'
+import Whatsapp from '../components/Whatsapp/Button'
 export default function Home() {
   return (
     <div>
@@ -11,9 +12,17 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header></Header>
-    <Banner></Banner>
-    <ServiciosGrid></ServiciosGrid>
+      <div className="relative">
+        <Banner></Banner>
+        <ServiciosGrid></ServiciosGrid>
+        <Whatsapp
+          numero="+56977031115"
+          msg="Hola Aravena y Asociados, necesito de sus servicios"
+        ></Whatsapp>
+      </div>
+      
     <Footer></Footer>
+    
     </div>
   )
 }
