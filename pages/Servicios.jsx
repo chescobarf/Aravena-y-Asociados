55a4d7f42a8d7ef501a0349edef300122187687d
Christian Escobar <christian.e@rompecabeza.cl>
2021-02-16 19:22:23 -0300

    change api route

--- pages/Servicios.jsx
+++ pages/Servicios.jsx
@@ -10,7 +10,8 @@ function Cotizar() {
 
   const fetchData = () => {
     // fetch("http://localhost:3000/api/servicesArray")
-    fetch("https://aravena-y-asociados.netlify.app/api/servicesArray")
+    // fetch("https://aravena-y-asociados.netlify.app/api/servicesArray")
+    fetch("https://aravena-y-asociados.vercel.app/api/servicesArray")
       .then((response) => response.json())
       .then((data) => setServicios(data))
       .catch((error) => console.error("Error:", error));
